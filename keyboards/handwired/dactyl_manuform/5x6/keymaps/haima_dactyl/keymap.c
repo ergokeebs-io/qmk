8c48dfa0fd87c8e8fc743f84495397ffeb6ebf1b
Juanma Orta <jorta@auvik.com>
2023-07-14 07:56:07 +0200

    Add config layer: gui toggle key enabled

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/haima_dactyl/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/haima_dactyl/keymap.c
@@ -3,7 +3,8 @@
 enum custom_layers {
  _BASE,
  _DEV,
- _NAV
+ _NAV,
+ _CONF
 };
 
 enum custom_keycodes {
@@ -49,22 +50,39 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_LCTL, KC_Z, KC_X, KC_C, KC_V, KC_B,                  KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_BSLS, 
                   KC_LBRC, KC_RBRC,                                       KC_PLUS, KC_EQL, 
       
-      KC_SPC, SGUI(KC_A), KC_LALT, KC_SPC, KC_LGUI, MO(2),    MO(1), KC_ENT, KC_NO, LAG(KC_I), SGUI(KC_4), KC_NO),
+                                  KC_SPC,  SGUI(KC_A),              KC_LALT, KC_SPC, 
+                                  KC_LGUI, MO(_NAV),                MO(_DEV),   KC_ENT, 
+                                  XXXXXXX, LAG(KC_I),               SGUI(KC_4), XXXXXXX
+    ),
 	[_DEV] = LAYOUT_5x6(
-      KC_TRNS, PIPE,  AT,    HASH,  DOLLAR, PERCENT,                CIRC,  AMP,     ASTERIX, LPARENS, RPARENS, DOG, 
-      KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO,  FUNCTION_START,         KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, 
-      KC_TRNS, KC_1, KC_2, KC_3, KC_4, KC_5,                        KC_6,  KC_7,    KC_8,    KC_9,    KC_0,    DBL_QUOTE, 
-      KC_TRNS, KC_NO, LCURLY, RCURLY, KC_NO, KC_NO,                        KC_NO, KC_NO,   LESSER,  GREATER, SLASH_QUEST,   KC_NO, 
-                   LSQUARE_CURLY, RSQUARE_CURLY,                               KC_NO,   EQUAL_PLUS, 
-      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+      _______, PIPE,  AT,    HASH,  DOLLAR, PERCENT,                CIRC,  AMP,     ASTERIX, LPARENS, RPARENS, DOG, 
+      _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  FUNCTION_START,         XXXXXXX, XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX, 
+      _______, KC_1, KC_2, KC_3, KC_4, KC_5,                        KC_6,  KC_7,    KC_8,    KC_9,    KC_0,    DBL_QUOTE, 
+      _______, XXXXXXX, LCURLY, RCURLY, XXXXXXX, XXXXXXX,                        XXXXXXX, XXXXXXX,   LESSER,  GREATER, SLASH_QUEST,   XXXXXXX, 
+                   LSQUARE_CURLY, RSQUARE_CURLY,                               XXXXXXX,   EQUAL_PLUS, 
+                                  _______, _______,                             _______, _______, 
+                                  _______, MO(_CONF),                            _______ , _______, 
+                                  XXXXXXX, _______,                             _______, XXXXXXX
     ),
 	[_NAV] = LAYOUT_5x6(
-      KC_TRNS, PIPE, AT, HASH, DOLLAR, PERCENT,                CIRC,    AMP,     ASTERIX, LPARENS,   RPARENS, KC_DEL, 
-      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_UP,   KC_TRNS,   KC_VOLD, KC_VOLU, 
-      KC_TRNS, KC_TRNS, SGUI(KC_S), KC_TRNS, KC_TRNS, KC_TRNS,    KC_PGUP, KC_LEFT, KC_DOWN, KC_RGHT,   KC_RGHT, DBL_QUOTE, 
-      KC_TRNS, SGUI(KC_Z), SGUI(KC_X), SGUI(KC_C), SGUI(KC_V), KC_TRNS,    KC_PGDN, KC_TRNS, LESSER,  GREATER,   SLASH_QUEST,   KC_NO,
-                        LSQUARE_CURLY, RSQUARE_CURLY,                                        KC_TRNS, EQUAL_PLUS, 
-      KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+      _______, PIPE, AT, HASH, DOLLAR, PERCENT,                CIRC,    AMP,     ASTERIX, LPARENS,   RPARENS, KC_DEL, 
+      _______, _______, _______, _______, _______, _______,    _______, _______, KC_UP,   _______,   KC_VOLD, KC_VOLU, 
+      _______, _______, SGUI(KC_S), _______, _______, _______,    KC_PGUP, KC_LEFT, KC_DOWN, KC_RGHT,   KC_RGHT, DBL_QUOTE, 
+      _______, SGUI(KC_Z), SGUI(KC_X), SGUI(KC_C), SGUI(KC_V), _______,    KC_PGDN, _______, LESSER,  GREATER,   SLASH_QUEST,   XXXXXXX,
+                        LSQUARE_CURLY, RSQUARE_CURLY,                                        _______, EQUAL_PLUS, 
+                                        _______, _______,                       _______, _______, 
+                                        _______, _______,                       MO(_CONF), _______, 
+                                        _______, _______,                       _______, _______
+    ),
+    [_CONF] = LAYOUT_5x6(
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, 
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, 
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, CG_TOGG,         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, 
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX,   XXXXXXX,
+                        XXXXXXX, XXXXXXX,                                        XXXXXXX, XXXXXXX, 
+                                    XXXXXXX, XXXXXXX,               XXXXXXX, XXXXXXX, 
+                                    XXXXXXX, _______,               _______, XXXXXXX,
+                                    _______, XXXXXXX,               XXXXXXX, _______
     )
 };
 
