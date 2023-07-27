dd6198dcf6f932696f9658b5200928e2f2e4d3ac
Juanma Orta <jorta@auvik.com>
2023-07-27 11:05:59 +0200

    Base config before removing custom functions

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/haima_dactyl/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/haima_dactyl/keymap.c
@@ -50,9 +50,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_LCTL, KC_Z, KC_X, KC_C, KC_V, KC_B,                  KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_BSLS, 
                   KC_LBRC, KC_RBRC,                                       KC_PLUS, KC_EQL, 
       
-                                  KC_SPC,  SGUI(KC_A),              KC_LALT, KC_SPC, 
+                                  KC_SPC,  LCTL(KC_C),              KC_LALT, KC_SPC, 
                                   KC_LGUI, MO(_NAV),                MO(_DEV),   KC_ENT, 
-                                  XXXXXXX, LAG(KC_I),               SGUI(KC_4), XXXXXXX
+                                  XXXXXXX, LCTL(KC_V),               KC_PSCR, XXXXXXX
     ),
 	[_DEV] = LAYOUT_5x6(
       _______, PIPE,    AT,      HASH,    DOLLAR,   PERCENT,                CIRC,    AMP,       ASTERIX,   LPARENS,   RPARENS,   DOG, 
@@ -65,24 +65,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                   XXXXXXX, _______,                             _______, XXXXXXX
     ),
 	[_NAV] = LAYOUT_5x6(
-      _______, PIPE, AT,   HASH, DOLLAR, PERCENT,                 CIRC,    AMP,     ASTERIX, LPARENS,   RPARENS, KC_DEL, 
-      _______, KC_1, KC_2, KC_3, KC_4,   KC_5,                    KC_6,    KC_7,    KC_8,    KC_9,      KC_0,    XXXXXXX, 
-      _______, _______, SGUI(KC_S), _______, _______, _______,    KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,  KC_VOLU, KC_VOLD, 
-      _______, SGUI(KC_Z), SGUI(KC_X), SGUI(KC_C), SGUI(KC_V), _______,    KC_PGDN, _______, LESSER,  GREATER,   SLASH_QUEST,   XXXXXXX,
-                        LSQUARE_CURLY, RSQUARE_CURLY,                                        _______, EQUAL_PLUS, 
+      _______, PIPE, AT,   HASH, DOLLAR, PERCENT,                 CIRC,    AMP,     ASTERIX, LPARENS,   RPARENS,   KC_DEL, 
+      _______, KC_1, KC_2, KC_3, KC_4,   KC_5,                    KC_6,    KC_7,    KC_8,    KC_9,      KC_0,      KC_VOLU, 
+      _______, _______, _______, _______, _______, _______,       KC_PGUP, KC_LEFT, KC_DOWN, KC_UP,     KC_RIGHT,  KC_VOLD, 
+      _______, _______, _______, _______, _______, _______,       KC_PGDN, _______, LESSER,  GREATER,   SLASH_QUEST,   XXXXXXX,
+                        LSQUARE_CURLY, RSQUARE_CURLY,                                        _______,   EQUAL_PLUS, 
                                         _______, _______,                       _______, _______, 
                                         _______, _______,                       TG(_CONF), _______, 
                                         _______, _______,                       _______, _______
     ),
     [_CONF] = LAYOUT_5x6(
       _______, QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, _______, 
-      _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,         XXXXXXX, KC_P7 ,  KC_P8 ,  KC_P9,   XXXXXXX, XXXXXXX, 
-      _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, CG_TOGG,         XXXXXXX, KC_P4 ,  KC_P5 ,  KC_P6,   XXXXXXX, XXXXXXX, 
-      _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,         XXXXXXX, KC_P1 ,  KC_P2 ,  KC_P3,   XXXXXXX,   XXXXXXX,
+      _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,         XXXXXXX, KC_P7 ,  KC_P8 ,  KC_P9,   XXXXXXX,   XXXXXXX, 
+      _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, CG_TOGG,         XXXXXXX, KC_P4 ,  KC_P5 ,  KC_P6,   XXXXXXX,   XXXXXXX, 
+      _______, _______, _______, _______, _______, XXXXXXX,         XXXXXXX, KC_P1 ,  KC_P2 ,  KC_P3,   XXXXXXX,   XXXXXXX,
                         XXXXXXX, XXXXXXX,                                        KC_P0, XXXXXXX, 
                                     _______, _______,               _______, _______, 
                                     _______, _______,               _______, _______,
-                                    _______, XXXXXXX,               _______, _______
+                                    _______, _______,               _______, _______
     )
 };
 
