23ff053b105d1462a477f9745f8de67c25adc544
Juanma Orta <jorta@auvik.com>
2023-07-27 12:01:36 +0200

    Remove custom dev keys

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/haima_dactyl/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/haima_dactyl/keymap.c
@@ -51,8 +51,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                   XXXXXXX, LCTL(KC_V),               KC_PSCR, XXXXXXX
     ),
 	[_DEV] = LAYOUT_5x6(
-      _______, PIPE,    AT,      HASH,    DOLLAR,   PERCENT,                CIRC,    AMP,       ASTERIX,   LPARENS,   RPARENS,   DOG, 
-      _______, KC_B, XXXXXXX, XXXXXXX, XXXXXXX,  FUNCTION_START,            XXXXXXX, XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX, 
+      _______, PIPE,    AT,      HASH,    DOLLAR,   PERCENT,                CIRC,    AMP,       ASTERIX,   LPARENS,   RPARENS,   KC_BSPC, 
+      _______, KC_B, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,            XXXXXXX, XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX, 
       _______, KC_1, KC_2,    KC_3,    KC_4,     KC_5,                      KC_6,    KC_7,      KC_8,      KC_9,      KC_0,      DBL_QUOTE, 
       _______, XXXXXXX, RALT(KC_QUOT), RALT(KC_BSLS), XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX,KC_GRV,    KC_TILD, SLASH_QUEST,   XXXXXXX, 
                         LSQUARE_CURLY, RSQUARE_CURLY,                               XXXXXXX,   EQUAL_PLUS, 
